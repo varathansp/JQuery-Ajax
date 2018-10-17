a8c4cf51cd0ad17bbb7f7e3949186efd512b08ef
Varathan <varathansuyambu95@gmail.com>
2018-10-17 13:01:53 +0530

    JQuery Ajax suggesting username project added

---
+++ SuggestingUserName/SuggestingUserName/Registration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuggestingUserName
+{
+    public class Registration
+    {
+
+        public string UserName { get; set; }
+        public bool UserNameInUse { get; set; }
+    }
+}
---
+++ SuggestingUserName/SuggestingUserName/RegistrationService.asmx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Web.Script.Serialization;
+
+namespace SuggestingUserName
+{
+    /// <summary>
+    /// Summary description for RegistrationService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class RegistrationService : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public bool UserNameExists(string userName)
+        {
+  
+
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            OleDbConnection connection = new OleDbConnection(cs);
+            connection.Open();
+            OleDbCommand command = new OleDbCommand("select * from tblRegistration where Username='" + userName + "'", connection);
+
+            return Convert.ToBoolean(command.ExecuteScalar());
+            
+        }
+
+        [WebMethod]
+        public void GetAvailableUserName(string userName)
+        {
+            Registration regsitration = new Registration();
+            regsitration.UserNameInUse = false;
+
+            while (UserNameExists(userName))
+            {
+                Random random = new Random();
+                int randomNumber = random.Next(1, 100);
+                userName = userName + randomNumber.ToString();
+                regsitration.UserNameInUse = true;
+            }
+
+            regsitration.UserName = userName;
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(regsitration));
+        }
+    }
+}
